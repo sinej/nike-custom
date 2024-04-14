6cc8ac065fa2d82a4a9ab940c01b4ec2d954fdf8
shin <sinej1020@gmail.com>
2024-04-14 23:11:16 +0900

    fetcher 수정

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -8,9 +8,25 @@ export const getClient = (() => {
     }
 })();
 
+type AnyOBJ = { [key: string]: any}
 
-export const fetcher = async ({ url = "", fetchOptions = {}}: string, {}) => {
+
+const BASE_URL = "";
+export const fetcher = async ({ method, path, body, params }: {
+    method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+    path: string;
+    body?: AnyOBJ;
+    params?: AnyOBJ
+}) => {
     try {
+        const url = `${BASE_URL}${path}`;
+        const fetchOptions: RequestInit = {
+            method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': BASE_URL
+            },
+        }
         const res = await fetch(url, fetchOptions);
         const json = await res.json();
         return json
